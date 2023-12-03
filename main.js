a11a836a18d24af57a19ea105306011b4b3ba415
Mohamed Fathi <mohamedfathielqastawy@gmail.com>
2023-12-03 08:02:21 +0200

    setup files

---
+++ main.js
@@ -0,0 +1,46 @@
+// 1- Write a program that allow to user enter number then print it
+
+// var num = prompt("enter your num");
+// console.log(num)
+
+// ========================================================================================================================
+
+// 2- Write a program that take number from user then print yes if that number can divide by 3 and 4 otherwise print no
+
+// var canDivide = prompt("is the can divide by 3 and 4")
+
+// if (canDivide%3===0 && canDivide%4===0) {
+//   console.log("yes")
+// } else {
+//   console.log("no")
+// }
+
+// ========================================================================================================================
+
+// 3- Write a program that allows the user to insert 2 integers then print the max
+
+// var num1 = prompt("Enter first number");
+// var num2 = prompt("Enter second number");
+
+// if (num1 > num2) {
+//   console.log(num1)
+// } else if (num1 < num2) {
+//   console.log(num2)
+// }
+
+// ========================================================================================================================
+
+// 4- Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+
+// var num = prompt("Enter number");
+
+// if (num>=0) {
+//   console.log("positive")
+// } else if (num<0) {
+//   console.log("negative")
+// }
+
+
+// ========================================================================================================================
+
+// ========================================================================================================================
